cf89cb6baaf4b99efa90e69bd5252a55bca05d09
ahmed.fuad@ona.io <ahmed.fuad@ona.io>
2023-03-01 19:49:20 +0000

    label changes

--- models/marts/hotspot_item_types.sql
+++ models/marts/hotspot_item_types.sql
@@ -20,9 +20,29 @@ with item_distribution as (
             {%- endif -%}       
         {% endfor %}]) as item_count
     from {{ ref('int_items_distributed') }}
+), item_label as (
+    select
+        item_type as eng,
+        label.label as item_type
+    from item_distribution
+    left join {{ ref('stg_hotspot_labels') }} label on label."name" = item_distribution.item_type
+    where label.list_name = 'item_type'
+    group by 1,2
 )
 
 select
-    *
+    id,
+    hotspot_id,
+    health_region,
+    health_district,
+    region,
+    district,
+    department,
+    commune_prefecture,
+    population_type,
+    today,
+    item_l.item_type,
+    item_count
 from item_distribution
+left join item_label item_l on item_distribution.item_type = item_l.eng
 where item_count is not null
